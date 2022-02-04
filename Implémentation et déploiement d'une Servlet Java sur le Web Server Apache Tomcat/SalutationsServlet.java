876c9ca02353d494dba01187b903bec4f4cea555
joelledaou <90617075+joelledaou@users.noreply.github.com>
2022-02-05 01:37:49 +0200

    Add files via upload

---
+++ Implémentation et déploiement d'une Servlet Java sur le Web Server Apache Tomcat/SalutationsServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/salutations"})
+
+public class SalutationsServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public void service( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		response.setContentType( "text/html" ); 
+		PrintWriter out = response.getWriter(); 
+		String nom = request.getParameter( "nom" ); 
+		String prenom = request.getParameter( "prenom" ); 
+		out.println( "<html><body>" ); 
+		out.println( "<h1>Exemple de résultat</h1>" ); 
+		out.println( "Bonjour " + prenom + " " + nom ); 
+		out.println( "</body></html>" ); 
+	} 
+
+}
+
+
