d145dae7ede668da5ebcefbddcfea59ca2c2a151
joelledaou <joelle.daou@net.usj.edu.lb>
2022-03-31 15:10:57 +0300

    Second commit: improvements

--- EJB Stateless Session Bean/src/main/java/com/example/ejb_statelesssessionbean/HelloServlet.java
+++ EJB Stateless Session Bean/src/main/java/com/example/ejb_statelesssessionbean/HelloServlet.java
@@ -1,23 +1,19 @@
 package com.example.ejb_statelesssessionbean;
+
 import java.io.*;
+import javax.ejb.EJB;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-    private HelloResource myStatelessSessionBean = new HelloResource();
-
-    public void init() {
-        message = myStatelessSessionBean.hello();
-    }
-
+    @EJB
+    private Hello helloBean = new HelloBean();
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + helloBean.sayHello() + "</h1>");
         out.println("</body></html>");
     }
 
